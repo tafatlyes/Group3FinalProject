0387f8b3b3eba4899982ad5d0c07e31239a87a32
tafatlyes <tafat.lyes54@gmail.com>
2020-12-05 13:25:19 -0500

    update

--- tdbank/src/test/java/com/tdbanktest/HomePageValidations.java
+++ tdbank/src/test/java/com/tdbanktest/HomePageValidations.java
@@ -3,59 +3,58 @@ package com.tdbanktest;
 import com.tdbank.pages.HomePage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pnt.automation.base.TestBase;
 
 public class HomePageValidations extends TestBase {
+    private HomePage homePage;
+
+    @BeforeMethod
+    public void setUpPageFactory(){
+         homePage = PageFactory.initElements(driver, HomePage.class);
+    }
 
 
     @Test
     public void validateUserBeingAbleToClickOnLogInButton() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnLogInButton();
     }
 
     @Test
     public void validateUserBeingAbleToClickOnSmallBusinessButton() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnSmallBusinessButton();
     }
 
     @Test
     public void validateUserBeingAbleToClickOnCommercialButton() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnCommercialButton();
     }
 
     @Test
     public void validateUserBeingAbleToClickOnInvestingAndWealthButton() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnInvestingAndWealthButton();
     }
 
 
     @Test
     public void validateUserBeingAbleToClicOnMortgageUnderProducts() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnMortgageUnderProducts();
     }
 
     @Test
     public void validateUserBeingAbleToClickOnCdsUnderProducts() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnCdsUnderProducts();
     }
 
 
     @Test
     public void validateUserBeingAbleToClickOnHomeEquityButton() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.clickOnHomeEquityButton();
     }
 
     @Test
     public void validateGetFinancialTipsTextDisplayed() {
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.getFinancialTipsTextDisplayed();
         Assert.assertTrue(true);
     }
